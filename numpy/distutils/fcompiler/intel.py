036df88f8524bf67b3dca4ae744b734316861c5b
ralex0 <ralex0@users.noreply.github.com>
2017-06-21 16:01:55 -0400

    Edit fcompiler/intel.py
    Chaged /MD compiler option to /MT to allow static linking in order to build a windows distribution of holopy.

--- numpy/distutils/fcompiler/intel.py
+++ numpy/distutils/fcompiler/intel.py
@@ -163,7 +163,7 @@ class IntelVisualFCompiler(BaseIntelFCompiler):
     module_include_switch = '/I'
 
     def get_flags(self):
-        opt = ['/nologo', '/MD', '/nbs', '/names:lowercase', '/assume:underscore']
+        opt = ['/nologo', '/MT', '/nbs', '/names:lowercase', '/assume:underscore']
         return opt
 
     def get_flags_free(self):
